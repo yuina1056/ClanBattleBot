f7f56133a34c744760c1d6f84d506ee911ae5c1d
yuina <yuina.i.1056@gmail.com>
2023-07-15 10:44:03 +0900

    ignore

--- src/commands/button/declaration.ts
+++ src/commands/button/declaration.ts
@@ -1,10 +1,12 @@
-import { ButtonBuilder, ButtonStyle, ButtonInteraction, Guild } from 'discord.js';
+import { ButtonBuilder, ButtonStyle, ButtonInteraction, Guild, ActionRowBuilder } from 'discord.js';
 export const customId = 'declaration'
 export const data = new ButtonBuilder()
   .setCustomId(customId)
   .setStyle(ButtonStyle.Primary)
   .setLabel("凸宣言")
 
+import button_declaration from '../button/declaration';
+import button_remainingHP from '../button/remaining_hp';
 
 export async function execute(interaction: ButtonInteraction) {
   let guild: Guild
@@ -14,7 +16,19 @@ export async function execute(interaction: ButtonInteraction) {
     return
   }
   const user = guild.members.cache.get(interaction.user.id)
-  await interaction.reply({ content: user?.nickname + 'が凸宣言しました' });
+
+  await interaction.channel?.send({ content: user?.nickname + 'が凸宣言しました' });
+
+  
+
+  await interaction.reply({
+    content: '結果を選択してください',
+    components: [
+      new ActionRowBuilder().addComponents(button_declaration.data, button_remainingHP.data).toJSON() as any,
+    ],
+    ephemeral: true
+  });
+
 }
 
 export default {
