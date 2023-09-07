949cbed403e38a49b8c2d57c720e05e0aa47e3dc
yuina <yuina.i.1056@gmail.com>
2023-09-07 20:26:10 +0900

    凸宣言仮実装

--- src/commands/button/declaration_cancel.ts
+++ src/commands/button/declaration_cancel.ts
@@ -1,4 +1,10 @@
 import { ButtonBuilder, ButtonStyle, ButtonInteraction, Guild } from 'discord.js';
+
+import DataSource from '../../datasource';
+import User from '../../entity/User';
+import Boss from '../../entity/Boss';
+import Declaration from '../../entity/Declaration';
+
 export const customId = 'declaration_cancel'
 export const data = new ButtonBuilder()
   .setCustomId(customId)
@@ -13,8 +19,28 @@ export async function execute(interaction: ButtonInteraction) {
   } else {
     return
   }
-  const user = guild.members.cache.get(interaction.user.id)
-  await interaction.reply({ content: user?.nickname + 'が凸宣言取消しました' });
+  // ボス情報取得
+  const bossRepository = DataSource.getRepository(Boss)
+  const boss = await bossRepository.findOneBy({ discordChannelId: interaction.channel!.id })
+  if (boss == null) {
+    return
+  }
+  // ユーザー取得
+  const userRepository = DataSource.getRepository(User)
+  const user = await userRepository.findOneBy({ discordUserId: interaction.user.id })
+  if (user == null) {
+    return
+  }
+
+  // DBから削除
+  const declarationRepository = DataSource.getRepository(Declaration)
+  const declaration = await declarationRepository.findOneBy({ userId: user.id, isFinished: false })
+  if (declaration == null) {
+    return
+  }
+  await declarationRepository.delete(declaration.id!)
+
+  await interaction.reply({ content: user.name + 'が凸宣言取消しました' });
 }
 
 export default {
--- src/commands/button/declaration_start.ts
+++ src/commands/button/declaration_start.ts
@@ -1,4 +1,10 @@
 import { ButtonBuilder, ButtonStyle, ButtonInteraction, Guild } from 'discord.js';
+
+import DataSource from '../../datasource';
+import User from '../../entity/User';
+import Boss from '../../entity/Boss';
+import Declaration from '../../entity/Declaration';
+
 export const customId = 'declaration_start'
 export const data = new ButtonBuilder()
   .setCustomId(customId)
@@ -13,8 +19,25 @@ export async function execute(interaction: ButtonInteraction) {
   } else {
     return
   }
-  const user = guild.members.cache.get(interaction.user.id)
-  await interaction.reply({ content: user?.nickname + 'が凸宣言しました' });
+  // ボス情報取得
+  const bossRepository = DataSource.getRepository(Boss)
+  const boss = await bossRepository.findOneBy({ discordChannelId: interaction.channel!.id })
+  if (boss == null) {
+    return
+  }
+  // ユーザー取得
+  const userRepository = DataSource.getRepository(User)
+  const user = await userRepository.findOneBy({ discordUserId: interaction.user.id })
+  if (user == null) {
+    return
+  }
+
+  // DBに保存
+  const declaration = new Declaration(user.clanId, user.id!, boss.bossid, 0, false)
+  const declarationRepository = DataSource.getRepository(Declaration)
+  await declarationRepository.save(declaration)
+
+  await interaction.reply({ content: user.name + 'が凸宣言しました' });
 }
 
 export default {
