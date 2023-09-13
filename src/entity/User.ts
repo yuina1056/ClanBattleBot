3e467db31a2d29466311e9fd2eb7637fbc7eeb98
yuina <yuina.i.1056@gmail.com>
2023-09-13 21:41:17 +0900

    リファクタ

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -35,60 +35,56 @@ export default class User {
     }
     public getAttackStatus(event: Event | null): string {
         let res: string = this.name + ' [－/－/－]'
-        if (event == null) {
-            return res
+        if (event == null || this.reports == null || this.reports.length === 0) {
+            return res + ' (記録なし)'
         }
-        if (this.reports == null || this.reports.length === 0) {
-            return res
+        if (this.reports.length === 0) {
+            return res + ' (記録なし)'
+        }
+        const maxId = Math.max(...this.reports.map(report => report.id) as number[]);
+        const latestReport = this.reports.find(report => report.id === maxId);
+        const thisMonthReports = this.reports.filter((report) => {
+            return report.eventId == event.id
+        })
+
+        if (thisMonthReports.length === 0) {
+            return res + '(' + dayjs(latestReport?.CreatedAt!).format('MM/DD HH:mm') + ') 【当月凸なし】'
         } else {
-            const thisMonthReports = this.reports.filter((report) => {
-                return report.eventId == event.id
+            const todayReports = thisMonthReports.filter((report) => {
+                return report.day == event.getClanBattleDay()
             })
-            if (thisMonthReports.length !== 0) {
-                const todayReports = thisMonthReports.filter((report) => {
-                    return report.day == event.getClanBattleDay()
-                })
-                if (todayReports.length !== 0) {
-                    // 当日凸あり
-                    res = this.name + ' ['
-                    for (let index = 1; index <= 3; index++) {
-                        const todayAttackCountReports = todayReports.filter(report => report.attackCount === index)
-                        if (todayAttackCountReports.length === 0) {
-                            // 凸なし
-                            res += '－'
+            if (todayReports.length === 0) {
+                return res
+            }
+            // 当日凸あり
+            res = this.name + ' ['
+            for (let index = 1; index <= 3; index++) {
+                const todayAttackCountReports = todayReports.filter(report => report.attackCount === index)
+                if (todayAttackCountReports.length === 0) {
+                    // 凸なし
+                    res += '－'
+                } else {
+                    // 凸あり
+                    let resDefeat: string = ''
+                    let resShave: string = ''
+                    todayAttackCountReports.forEach(report => {
+                        if (!report.isCarryOver) {
+                            resShave = report.bossId + '削り'
                         } else {
-                            // 凸あり
-                            if (todayAttackCountReports.length === 0) {
-                                // 凸なし
-                                res += '－'
-                            } else {
-                                // 凸あり
-                                let resDefeat: string = ''
-                                let resShave: string = ''
-                                todayAttackCountReports.forEach(report => {
-                                    if (!report.isCarryOver) {
-                                        resShave = report.bossId + '削り'
-                                    } else {
-                                        resDefeat += report.bossId + '撃破'
-                                    }
-                                })
-                                if (resDefeat === '') {
-                                    res += resShave
-                                } else {
-                                    res += resDefeat + resShave
-                                }
-                            }
+                            resDefeat += report.bossId + '撃破'
                         }
-                        if (index !== 3) {
-                            res += '/'
-                        }
+                    })
+                    if (resDefeat === '') {
+                        res += resShave
+                    } else {
+                        res += resDefeat + resShave
                     }
-                    res += ']'
+                }
+                if (index !== 3) {
+                    res += '/'
                 }
             }
-            const maxId = Math.max(...this.reports.map(report => report.id) as number[]);
-            const latestReport = this.reports.find(report => report.id === maxId);
-            res += '('+dayjs(latestReport?.CreatedAt!).format('MM/DD HH:mm')+')'
+            res += '] ('+dayjs(latestReport?.CreatedAt!).format('MM/DD HH:mm')+')'
         }
         return res
     }
