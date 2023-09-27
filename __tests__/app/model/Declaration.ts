87888130182e7315e6a259a901c9b464906da86d
Daisuke Miura <mhden@drakontia.com>
2023-09-27 03:11:51 +0000

    頑張った形跡

--- __tests__/app/model/Declaration.ts
+++ __tests__/app/model/Declaration.ts
@@ -1,24 +1,23 @@
 
 import { describe, expect, test } from "@jest/globals"
-import dataSource from "@/datasource"
-import declaration from "@/app/model/Declaration"
-import Boss from "@/entity/Boss"
-import Declaration from "@/entity/Declaration"
-import { load } from "../../../loadFixtures"
+import dataSource from "../../../src/datasource"
+import declaration from "../../../src/app/model/Declaration"
+import Boss from "../../../src/entity/Boss"
+import Declaration from "../../../src/entity/Declaration"
+import { loadFixtures } from "../../loadfixtures"
 
-    jest.useFakeTimers();
+// jest.useFakeTimers();
 
-beforeEach(() => {
-
-    load
+beforeEach( async () => {
+    loadFixtures("../../../fixtures")
 })
 
 afterEach(async () => {
     await dataSource.destroy()
 })
 
-describe('クランバトル開催期間外', async () => {
-    jest.setSystemTime(new Date('2023-09-01T12:00:00.000+09:00'))
+describe('クランバトル開催期間外', () => {
+    // jest.setSystemTime(new Date('2023-09-01T12:00:00.000+09:00'))
 
     test('test', async () => {
         const boss = new Boss(
@@ -26,21 +25,19 @@ describe('クランバトル開催期間外', async () => {
             1,
             "1"
         )
-        const data = declaration.regist(boss, "1", 1)
-        expect(data).toThrow("クランバトル開催情報が取得できませんでした");
+        expect(declaration.regist(boss, "1", 1)).toThrow("クランバトル開催情報が取得できませんでした");
     })
 })
 
-describe('無効ユーザ', async () => {
-    jest.setSystemTime(new Date("2023-09-25T12:00:00.000+09:00"))
+describe('無効ユーザ', () => {
+    // jest.setSystemTime(new Date("2023-09-25T12:00:00.000+09:00"))
 
     const boss = new Boss(1, 1, "1")
-    const data = declaration.regist(boss, "nopermissionuser", 1)
-    expect(data).toThrow("ユーザー情報が取得できませんでした");
+    expect(() => declaration.regist(boss, "nopermissionuser", 1)).toThrow("ユーザー情報が取得できませんでした");
 })
 
-describe('重複凸宣言', async () => {
-    jest.setSystemTime(new Date("2023-09-25T12:00:00.000+09:00"));
+describe('重複凸宣言', () => {
+    // jest.setSystemTime(new Date("2023-09-25T12:00:00.000+09:00"));
 
     const declarationRepository = dataSource.getRepository(Declaration)
     const pastDeclaration = new Declaration(
@@ -56,32 +53,29 @@ describe('重複凸宣言', async () => {
     declarationRepository.save(pastDeclaration)
 
     const boss = new Boss(1, 1, "1");
-    const data = declaration.regist(boss, "1", 1);
-    expect(data).toThrow("既に1ボスに凸宣言済みです");
+    expect(declaration.regist(boss, "1", 1)).toThrow("既に1ボスに凸宣言済みです");
 })
 
-describe("重複凸持ち越し含む", async () => {
-  jest.setSystemTime(new Date("2023-09-25T12:00:00.000+09:00"));
+describe("重複凸持ち越し含む", () => {
+    // jest.setSystemTime(new Date("2023-09-25T12:00:00.000+09:00"));
 
-  const declarationRepository = dataSource.getRepository(Declaration);
+    const declarationRepository = dataSource.getRepository(Declaration);
     const pastDeclaration1 = new Declaration(1, 1, 1, 1, 0, 1, 1, false);
     const pastDeclaration2 = new Declaration(1, 1, 1, 1, 0, 1, 1, true);
     declarationRepository.save(pastDeclaration1);
     declarationRepository.save(pastDeclaration2);
 
-  const boss = new Boss(1, 1, "1");
-  const data = declaration.regist(boss, "1", 1);
-  expect(data).toThrow("既に1凸目は完了しています");
+    const boss = new Boss(1, 1, "1");
+    expect(declaration.regist(boss, "1", 1)).toThrow("既に1凸目は完了しています");
 });
 
-describe("重複凸持ち越しなし", async () => {
-  jest.setSystemTime(new Date("2023-09-25T12:00:00.000+09:00"));
+describe("重複凸持ち越しなし", () => {
+    // jest.setSystemTime(new Date("2023-09-25T12:00:00.000+09:00"));
 
-  const declarationRepository = dataSource.getRepository(Declaration);
-  const pastDeclaration = new Declaration(1, 1, 1, 1, 0, 1, 1, true);
-  declarationRepository.save(pastDeclaration);
+    const declarationRepository = dataSource.getRepository(Declaration);
+    const pastDeclaration = new Declaration(1, 1, 1, 1, 0, 1, 1, true);
+    declarationRepository.save(pastDeclaration);
 
-  const boss = new Boss(1, 1, "1");
-  const data = declaration.regist(boss, "1", 1);
-  expect(data).toThrow("既に1凸目は完了しています");
+    const boss = new Boss(1, 1, "1");
+    expect(declaration.regist(boss, "1", 1)).toThrow("既に1凸目は完了しています");
 });
