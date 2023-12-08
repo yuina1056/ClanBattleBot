37d0721a625653e0bf358c6639b4b035439af3f6
Natsuki Wada <yuina.i.1056@gmail.com>
2023-12-08 23:30:55 +0900

    段階情報をconfigとして持っていろいろやる (#68)
    * コンフィグファイルの作成
    
    * 周回数バリデーションを段階に対応
    
    * doc追加
    
    * 境界値からむのでテスト追加しておく
    
    * docも追加しておく
    
    * 凸状況を見やすくするために10文字分空白で埋める
    
    * ちょい差し替え

--- src/entity/Lap.ts
+++ src/entity/Lap.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import Config from "@/config/config";
 
 @Entity()
 // クランバトル周回数
@@ -25,54 +26,174 @@ export default class Lap {
     this.eventId = eventId;
   }
 
+  /**
+   * ボスに攻撃可能かを判定する
+   *
+   * @param bossId ボスID(1~5)
+   * @return true:攻撃可能 false:攻撃不可
+   */
   isAttackPossible(bossId: number): boolean {
+    if (
+      this.boss1Lap == null ||
+      this.boss2Lap == null ||
+      this.boss3Lap == null ||
+      this.boss4Lap == null ||
+      this.boss5Lap == null
+    ) {
+      throw new Error("lap.boss1Lap is null");
+    }
     switch (bossId) {
       case 1:
+        // 周回差チェック
         if (
-          this.boss1Lap! - this.boss2Lap! >= 2 ||
-          this.boss1Lap! - this.boss3Lap! >= 2 ||
-          this.boss1Lap! - this.boss4Lap! >= 2 ||
-          this.boss1Lap! - this.boss5Lap! >= 2
+          this.boss1Lap - this.boss2Lap >= 2 ||
+          this.boss1Lap - this.boss3Lap >= 2 ||
+          this.boss1Lap - this.boss4Lap >= 2 ||
+          this.boss1Lap - this.boss5Lap >= 2
+        ) {
+          return false;
+        }
+        // 3段階をまたぐボスチェック
+        if (
+          this.boss1Lap == Config.LapConfig.bossLap[3] - 1 &&
+          this.boss2Lap < Config.LapConfig.bossLap[3] &&
+          this.boss3Lap < Config.LapConfig.bossLap[3] &&
+          this.boss4Lap < Config.LapConfig.bossLap[3] &&
+          this.boss5Lap < Config.LapConfig.bossLap[3]
+        ) {
+          return false;
+        }
+        // 4段階をまたぐボスチェック
+        if (
+          this.boss1Lap == Config.LapConfig.bossLap[4] - 1 &&
+          this.boss2Lap < Config.LapConfig.bossLap[4] &&
+          this.boss3Lap < Config.LapConfig.bossLap[4] &&
+          this.boss4Lap < Config.LapConfig.bossLap[4] &&
+          this.boss5Lap < Config.LapConfig.bossLap[4]
         ) {
           return false;
         }
         break;
       case 2:
+        // 周回差チェック
         if (
-          this.boss2Lap! - this.boss1Lap! >= 2 ||
-          this.boss2Lap! - this.boss3Lap! >= 2 ||
-          this.boss2Lap! - this.boss4Lap! >= 2 ||
-          this.boss2Lap! - this.boss5Lap! >= 2
+          this.boss2Lap - this.boss1Lap >= 2 ||
+          this.boss2Lap - this.boss3Lap >= 2 ||
+          this.boss2Lap - this.boss4Lap >= 2 ||
+          this.boss2Lap - this.boss5Lap >= 2
+        ) {
+          return false;
+        }
+        // 3段階をまたぐボスチェック
+        if (
+          this.boss2Lap == Config.LapConfig.bossLap[3] - 1 &&
+          this.boss1Lap < Config.LapConfig.bossLap[3] &&
+          this.boss3Lap < Config.LapConfig.bossLap[3] &&
+          this.boss4Lap < Config.LapConfig.bossLap[3] &&
+          this.boss5Lap < Config.LapConfig.bossLap[3]
+        ) {
+          return false;
+        }
+        // 4段階をまたぐボスチェック
+        if (
+          this.boss2Lap == Config.LapConfig.bossLap[4] - 1 &&
+          this.boss1Lap < Config.LapConfig.bossLap[4] &&
+          this.boss3Lap < Config.LapConfig.bossLap[4] &&
+          this.boss4Lap < Config.LapConfig.bossLap[4] &&
+          this.boss5Lap < Config.LapConfig.bossLap[4]
         ) {
           return false;
         }
         break;
       case 3:
+        // 周回差チェック
         if (
-          this.boss3Lap! - this.boss1Lap! >= 2 ||
-          this.boss3Lap! - this.boss2Lap! >= 2 ||
-          this.boss3Lap! - this.boss4Lap! >= 2 ||
-          this.boss3Lap! - this.boss5Lap! >= 2
+          this.boss3Lap - this.boss1Lap >= 2 ||
+          this.boss3Lap - this.boss2Lap >= 2 ||
+          this.boss3Lap - this.boss4Lap >= 2 ||
+          this.boss3Lap - this.boss5Lap >= 2
+        ) {
+          return false;
+        }
+        // 3段階をまたぐボスチェック
+        if (
+          this.boss3Lap == Config.LapConfig.bossLap[3] - 1 &&
+          this.boss1Lap < Config.LapConfig.bossLap[3] &&
+          this.boss2Lap < Config.LapConfig.bossLap[3] &&
+          this.boss4Lap < Config.LapConfig.bossLap[3] &&
+          this.boss5Lap < Config.LapConfig.bossLap[3]
+        ) {
+          return false;
+        }
+        // 4段階をまたぐボスチェック
+        if (
+          this.boss3Lap == Config.LapConfig.bossLap[4] - 1 &&
+          this.boss1Lap < Config.LapConfig.bossLap[4] &&
+          this.boss2Lap < Config.LapConfig.bossLap[4] &&
+          this.boss4Lap < Config.LapConfig.bossLap[4] &&
+          this.boss5Lap < Config.LapConfig.bossLap[4]
         ) {
           return false;
         }
         break;
       case 4:
+        // 周回差チェック
         if (
-          this.boss4Lap! - this.boss1Lap! >= 2 ||
-          this.boss4Lap! - this.boss2Lap! >= 2 ||
-          this.boss4Lap! - this.boss3Lap! >= 2 ||
-          this.boss4Lap! - this.boss5Lap! >= 2
+          this.boss4Lap - this.boss1Lap >= 2 ||
+          this.boss4Lap - this.boss2Lap >= 2 ||
+          this.boss4Lap - this.boss3Lap >= 2 ||
+          this.boss4Lap - this.boss5Lap >= 2
+        ) {
+          return false;
+        }
+        // 3段階をまたぐボスチェック
+        if (
+          this.boss4Lap == Config.LapConfig.bossLap[3] - 1 &&
+          this.boss1Lap < Config.LapConfig.bossLap[3] &&
+          this.boss2Lap < Config.LapConfig.bossLap[3] &&
+          this.boss3Lap < Config.LapConfig.bossLap[3] &&
+          this.boss5Lap < Config.LapConfig.bossLap[3]
+        ) {
+          return false;
+        }
+        // 4段階をまたぐボスチェック
+        if (
+          this.boss4Lap == Config.LapConfig.bossLap[4] - 1 &&
+          this.boss1Lap < Config.LapConfig.bossLap[4] &&
+          this.boss2Lap < Config.LapConfig.bossLap[4] &&
+          this.boss3Lap < Config.LapConfig.bossLap[4] &&
+          this.boss5Lap < Config.LapConfig.bossLap[4]
         ) {
           return false;
         }
         break;
       case 5:
+        // 周回差チェック
         if (
-          this.boss5Lap! - this.boss1Lap! >= 2 ||
-          this.boss5Lap! - this.boss2Lap! >= 2 ||
-          this.boss5Lap! - this.boss3Lap! >= 2 ||
-          this.boss5Lap! - this.boss4Lap! >= 2
+          this.boss5Lap - this.boss1Lap >= 2 ||
+          this.boss5Lap - this.boss2Lap >= 2 ||
+          this.boss5Lap - this.boss3Lap >= 2 ||
+          this.boss5Lap - this.boss4Lap >= 2
+        ) {
+          return false;
+        }
+        // 3段階をまたぐボスチェック
+        if (
+          this.boss5Lap == Config.LapConfig.bossLap[3] - 1 &&
+          this.boss1Lap < Config.LapConfig.bossLap[3] &&
+          this.boss2Lap < Config.LapConfig.bossLap[3] &&
+          this.boss3Lap < Config.LapConfig.bossLap[3] &&
+          this.boss4Lap < Config.LapConfig.bossLap[3]
+        ) {
+          return false;
+        }
+        // 4段階をまたぐボスチェック
+        if (
+          this.boss5Lap == Config.LapConfig.bossLap[4] - 1 &&
+          this.boss1Lap < Config.LapConfig.bossLap[4] &&
+          this.boss2Lap < Config.LapConfig.bossLap[4] &&
+          this.boss3Lap < Config.LapConfig.bossLap[4] &&
+          this.boss4Lap < Config.LapConfig.bossLap[4]
         ) {
           return false;
         }
