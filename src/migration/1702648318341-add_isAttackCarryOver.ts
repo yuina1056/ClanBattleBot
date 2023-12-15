29688f714c4c79ded6f1de596540483cf428cb92
yuina <yuina.i.1056@gmail.com>
2023-12-15 23:59:48 +0900

    fmt

--- src/migration/1702648318341-add_isAttackCarryOver.ts
+++ src/migration/1702648318341-add_isAttackCarryOver.ts
@@ -1,16 +1,17 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddIsAttackCarryOver1702648318341 implements MigrationInterface {
-    name = 'AddIsAttackCarryOver1702648318341'
+  name = "AddIsAttackCarryOver1702648318341";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`declaration\` ADD \`isAttackCarryOver\` tinyint NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`report\` ADD \`isAttackCarryOver\` tinyint NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`declaration\` ADD \`isAttackCarryOver\` tinyint NOT NULL`,
+    );
+    await queryRunner.query(`ALTER TABLE \`report\` ADD \`isAttackCarryOver\` tinyint NOT NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`report\` DROP COLUMN \`isAttackCarryOver\``);
-        await queryRunner.query(`ALTER TABLE \`declaration\` DROP COLUMN \`isAttackCarryOver\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`report\` DROP COLUMN \`isAttackCarryOver\``);
+    await queryRunner.query(`ALTER TABLE \`declaration\` DROP COLUMN \`isAttackCarryOver\``);
+  }
 }
