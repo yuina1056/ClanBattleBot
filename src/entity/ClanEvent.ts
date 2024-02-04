3e395527a6369234cef8a02aecc3d7032640725f
yuina <yuina.i.1056@gmail.com>
2024-02-04 20:47:38 +0900

    entity作成

---
+++ src/entity/ClanEvent.ts
@@ -0,0 +1,42 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+// クラン毎イベント情報
+export default class ClanEvent {
+  @PrimaryGeneratedColumn()
+  id?: number;
+  @Column()
+  clanId: number;
+  @Column()
+  eventId: number;
+
+  @Column({ default: 1 })
+  boss1Lap?: number;
+  @Column({ default: 800 })
+  boss1HP?: number;
+
+  @Column({ default: 1 })
+  boss2Lap?: number;
+  @Column({ default: 1000 })
+  boss2HP?: number;
+
+  @Column({ default: 1 })
+  boss3Lap?: number;
+  @Column({ default: 1300 })
+  boss3HP?: number;
+
+  @Column({ default: 1 })
+  boss4Lap?: number;
+  @Column({ default: 1500 })
+  boss4HP?: number;
+
+  @Column({ default: 1 })
+  boss5Lap?: number;
+  @Column({ default: 2000 })
+  boss5HP?: number;
+
+  constructor(clanId: number, eventId: number) {
+    this.clanId = clanId;
+    this.eventId = eventId;
+  }
+}
