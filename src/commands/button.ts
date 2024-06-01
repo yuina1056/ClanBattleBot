bad3ccbf32210f2f715004ff8c768219fa3f6373
yuina <yuina.i.1056@gmail.com>
2024-06-01 12:43:18 +0900

    ボタン実装

--- src/commands/button.ts
+++ src/commands/button.ts
@@ -18,6 +18,7 @@ import { ResetDeclarationReportSecond } from "@/commands/button/resetDeclaration
 import { ResetDeclarationReportThird } from "@/commands/button/resetDeclarationReportThird";
 import { EditLap } from "@/commands/button/editLap";
 import { EditHp } from "@/commands/button/editHp";
+import { Relief } from "@/commands/button/relief";
 
 export async function action(interaction: ButtonInteraction) {
   let action = null;
@@ -75,6 +76,9 @@ export async function action(interaction: ButtonInteraction) {
       break;
     case EditHp.customId:
       action = new EditHp();
+      break;
+    case Relief.customId:
+      action = new Relief();
       break;
     default:
       console.error(`${interaction.customId}というボタンには対応していません。`);
